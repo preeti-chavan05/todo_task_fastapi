429b3544d7a662677ce5386f736da72d31c9c49f
Preeti Chavan <preeti.p.c.tas@gmail.com>
2026-02-12 17:07:23 +0530

    db logic

--- database.py
+++ database.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = "sqlite:///./todo.db"
 
+
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
